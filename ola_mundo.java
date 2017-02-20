68365c31b5b486107a368696b707e3ba6a6cce02
Croisebeck <miguel_copatti@live.com>
2017-02-20 19:49:28 -0300

    adicionado menssagem em janela

--- ola_mundo.java
+++ ola_mundo.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
+
 public class ola_mundo
 {
 	public static void main (String[] Args)
 	{
-		System.out.println("Ola, Git!");
+		//System.out.println("Ola, Git!");
+		JOptionPane.showMessageDialog(null, " ola git!" );
 	}	
 }	
