2d4a39101fc89ed8883a1957482a23a7ac0827f9
croisebeck <miguel_copatti@live.com>
2017-02-20 23:34:10 -0300

    arrumada a string de saida

--- ola_mundo.java
+++ ola_mundo.java
@@ -5,6 +5,6 @@ public class ola_mundo
 	public static void main (String[] Args)
 	{
 		//System.out.println("Ola, Git!");
-		JOptionPane.showMessageDialog(null, " ola git!" );
+		JOptionPane.showMessageDialog(null, " ola git!!!" );
 	}	
 }	
